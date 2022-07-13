bf9981fe435d25eb5bc6e91a1ebe27874d5599e2
Odohofre <odohofrebright@gmail.com>
2022-07-13 19:15:32 +0100

    Recreate files deleted by mistake

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h> /* va_list */
+#include <unistd.h> /* write */
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int (*find_function(const char *format))(va_list);
+int print_str(va_list list);
+int print_char(va_list list);
+int print_cent();
+
+
+/**
+ * struct types - contains different types of format
+ * @sc: The specifiers
+ * @funct: The associated function
+ */
+typedef struct types
+{
+	char *sc;
+	int (*func)(va_list);
+} print;
+
+#endif
